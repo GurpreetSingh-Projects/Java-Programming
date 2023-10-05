eae0eacdf8853ce92b2ba7d90b2c550a93bd00df
GurpreetSingh-Projects <gpsingh02414@gmail.com>
2023-10-05 16:51:45 +0530

    code update

--- program 6.java
+++ program 6.java
@@ -1,62 +1,35 @@
-/**
-There is a programming language with only four operations and one variable X:
+import java.io.*;
+class BinarySearch {
 
-++X and X++ increments the value of the variable X by 1.
---X and X-- decrements the value of the variable X by 1.
-Initially, the value of X is 0.
-
-Given an array of strings operations containing a list of operations, return the final value of X after performing all the operations.
-
- 
-
-Example 1:
-
-Input: operations = ["--X","X++","X++"]
-Output: 1
-Explanation: The operations are performed as follows:
-Initially, X = 0.
---X: X is decremented by 1, X =  0 - 1 = -1.
-X++: X is incremented by 1, X = -1 + 1 =  0.
-X++: X is incremented by 1, X =  0 + 1 =  1.
-Example 2:
-
-Input: operations = ["++X","++X","X++"]
-Output: 3
-Explanation: The operations are performed as follows:
-Initially, X = 0.
-++X: X is incremented by 1, X = 0 + 1 = 1.
-++X: X is incremented by 1, X = 1 + 1 = 2.
-X++: X is incremented by 1, X = 2 + 1 = 3.
-Example 3:
-
-Input: operations = ["X++","++X","--X","X--"]
-Output: 0
-Explanation: The operations are performed as follows:
-Initially, X = 0.
-X++: X is incremented by 1, X = 0 + 1 = 1.
-++X: X is incremented by 1, X = 1 + 1 = 2.
---X: X is decremented by 1, X = 2 - 1 = 1.
-X--: X is decremented by 1, X = 1 - 1 = 0.
- 
-
-Constraints:
-
-1 <= operations.length <= 100
-operations[i] will be either "++X", "X++", "--X", or "X--". */
-class Solution {
-    public int finalValueAfterOperations(String[] str) {
-        int x=0;
-        for(int i=0;i<str.length;i++)
+	// Returns index of x if it is present in arr[].
+	int binarySearch(int arr[], int x)
+	{
+        int left=0, right=arr.length;
+        while(left<right)
         {
-            if(str[i].equals("--X"))
-            --x;
-            else if(str[i].equals("X++"))
-            x++;
-            else if(str[i].equals("++X"))
-            ++x;
+            int mid=(left+right-1)/2;
+            if(x==arr[mid])
+            return mid;
+            else if(arr[mid]<x)
+            left = mid + 1;
             else
-            x--;
+            right=mid-1;    
         }
-        return x;
-    }
+        return -1;
+	}
+	// Driver code
+	public static void main(String args[])
+	{
+		BinarySearch ob = new BinarySearch();
+		int arr[] = { 2, 3, 4, 10, 40 };
+		int n = arr.length;
+		int x = 10;
+		int result = ob.binarySearch(arr, x);
+		if (result == -1)
+			System.out.println(
+				"Element is not present in array");
+		else
+			System.out.println("Element is present at "
+							+ "index " + result);
+	}
 }
