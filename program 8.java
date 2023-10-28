396ce8be0bc1b47b6131d449834b634e1764fd04
GurpreetSingh-Projects <gpsingh02414@gmail.com>
2023-10-29 04:19:13 +0530

    update

---
+++ program 8.java
@@ -0,0 +1,14 @@
+import java.util.*;
+private class animal{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int num=sc.nextInt();
+
+    }
+    private void output(int n)
+    {
+        System.out.print(n);
+    }
+}
